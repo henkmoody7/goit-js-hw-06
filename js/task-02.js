19accc11fdfa9076af100013564f4baecb7d6fac
Mykola Harasym <henkmoody7@gmail.com>
2022-07-28 13:51:53 +0300

    task2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,29 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsListRef = document.querySelector("#ingredients");
+
+// const elements = ingredients.map((ingredient) => {
+//   const itemRef = document.createElement("li");
+//   itemRef.classList.add("item");
+//   itemRef.textContent = ingredient;
+//   ingredientsListRef.appendChild(itemRef);
+//   // console.log(itemRef);
+//   // return itemRef;
+// });
+
+const createIngredientsList = (arr) => {
+  return arr.map((ingredient) => {
+    const itemRef = document.createElement("li");
+    itemRef.classList.add("item");
+    itemRef.textContent = ingredient;
+    ingredientsListRef.appendChild(itemRef);
+    return itemRef;
+  });
+};
+createIngredientsList(ingredients);
